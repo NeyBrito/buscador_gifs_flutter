f6d6ae0d3458924f8c51f714a056f4650353ad42
Ney Brito <ney.brito@etec.sp.gov.br>
2021-06-16 15:30:46 -0300

    Commit final

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
+import 'package:buscador_gifs_flutter/ui/gif_page.dart';
 import 'package:flutter/material.dart';
+import 'package:buscador_gifs_flutter/ui/home_page.dart';
 
 void main(){
   runApp(MaterialApp(
-    
-  ));
+    home: HomePage(),
+    theme: ThemeData(
+      hintColor: Colors.white,
+      inputDecorationTheme: InputDecorationTheme(
+        border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white)),
+        enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white)),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white)),
+      ),
+    )
+   ),
+  );
 }
+
